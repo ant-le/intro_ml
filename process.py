01d05b380b87f113f721948c25c6ef482d7bb809
ant-le <lechuga.anton@gmail.com>
2022-10-22 22:56:00 +0200

    Plot Attributes

--- process.py
+++ process.py
@@ -3,12 +3,12 @@ from pathlib import Path
 import pandas as pd
 import matplotlib.pyplot as plt
 
-DATA_PATH = Path(__file__).parent / "data"
+DIR = Path(__file__).parent
 
-df1 = pd.read_csv(DATA_PATH.joinpath("winequality-red.csv"), sep=";")
+df1 = pd.read_csv(DIR / "data" / "winequality-red.csv", sep=";")
 df1.loc[:, "type"] = "red"
 
-df2 = pd.read_csv(DATA_PATH.joinpath("winequality-white.csv"), sep=";")
+df2 = pd.read_csv(DIR / "data" / "winequality-white.csv", sep=";")
 df2.loc[:, "type"] = "white"
 
 wine = pd.concat([df1, df2])
@@ -17,28 +17,24 @@ wine = pd.concat([df1, df2])
 print("Number of Samples: " + str(wine.shape[0]) + ", Number of Attributes: " + str(wine.shape[1]))
 print(wine.dtypes)
 
-# target attribute
+# frequency table for target attribute
 print(wine.quality.value_counts().sort_index().to_latex())
 
 # predictor attributes
 wine.type.value_counts() # binary type (nominal)
 
 with plt.style.context("seaborn-white"): # alcohol might be transformable
-    plt.hist(wine.alcohol, alpha=.8, color="lightgray", edgecolor="black", bins=15)
-    plt.xlabel("Alcohol")
+    plt.figure()
+    plt.hist(wine["citric acid"], alpha=.8, color="lightgray", edgecolor="black", bins=20)
+    plt.xlabel("Citric Acid")
     plt.ylabel("Frequency")
-    plt.show()
-    plt.savefig
+    plt.savefig(DIR / "images" / "acid.pdf")
 
 
 with plt.style.context("seaborn-white"): # alcohol might be transformable
+    plt.figure()
     plt.hist(wine.density, alpha=.8, color="lightgray", edgecolor="black", bins=40)
     plt.xlabel("Density")
     plt.ylabel("Frequency")
+    plt.savefig(DIR / "images" / "density.pdf")
     plt.show()
-    
-
-i=4
-plt.hist(wine.iloc[:,i], bins=20)
-plt.title("Histogram for " + wine.iloc[:,i].name)
-plt.show()
